1a33d0b792af86e4b849f06bae62b96290619abf
Gabriel Rodríguez <rgabriel@mit.edu>
2023-09-05 16:03:38 -0400

    add tooltip to MatrixLink

--- src/components/MatrixLink.tsx
+++ src/components/MatrixLink.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Tooltip } from "@chakra-ui/react";
 import { Activity } from "../lib/activity";
 import { Class } from "../lib/class";
 import { ChatIcon, ExternalLinkIcon } from "@chakra-ui/icons";
@@ -17,11 +17,14 @@ export function MatrixLink(props: { selectedActivities: Array<Activity> }) {
   return (
     <>
       <a href={matrixLink} target="_blank" rel="noreferrer">
-        <Button
-          colorScheme="teal"
-          leftIcon={<ChatIcon />}
-          rightIcon={<ExternalLinkIcon />}
-          size="sm">Join group chats on Matrix</Button>
+        <Tooltip
+          label="You will be able to choose which chats to join, if any.">
+          <Button
+            colorScheme="teal"
+            leftIcon={<ChatIcon />}
+            rightIcon={<ExternalLinkIcon />}
+            size="sm">Join group chats on Matrix</Button>
+        </Tooltip>
       </a>
     </>
   );
