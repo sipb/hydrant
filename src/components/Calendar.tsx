28a018378024020cac1b2a2ccbb1e190097a1f67
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-12-01 02:26:00 -0500

    rollback calendar changes, fix react and typescript

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,10 +1,6 @@
 import { Box, Text } from "@chakra-ui/react";
 import FullCalendar from "@fullcalendar/react";
-import type {
-  DateSelectArg,
-  EventClickArg,
-  EventContentArg,
-} from "@fullcalendar/core";
+import type { EventContentArg } from "@fullcalendar/core";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
@@ -13,7 +9,6 @@ import { Slot } from "../lib/dates";
 import { State } from "../lib/state";
 
 import "./Calendar.scss";
-import { useMemo } from "react";
 
 /**
  * Calendar showing all the activities, including the buttons on top that
@@ -26,27 +21,27 @@ export function Calendar(props: {
 }) {
   const { selectedActivities, viewedActivity, state } = props;
 
-  const events = useMemo(
-    () =>
-      selectedActivities
-        .flatMap((act) => act.events)
-        .flatMap((event) => event.eventInputs),
-    [selectedActivities],
-  );
-  const handleEventClick = (e: EventClickArg) => {
-    // extendedProps: non-standard props of {@link Event.eventInputs}
-    state.setViewedActivity(e.event.extendedProps.activity as Activity);
-  };
-
-  const handleSelect = (e: DateSelectArg) => {
-    viewedActivity instanceof NonClass &&
-      state.addTimeslot(
-        viewedActivity,
-        Timeslot.fromStartEnd(
-          Slot.fromStartDate(e.start),
-          Slot.fromStartDate(e.end),
-        ),
-      );
+  const renderEvent = ({ event }: EventContentArg) => {
+    return (
+      <Box
+        color={event.textColor}
+        p={0.5}
+        lineHeight={1.3}
+        cursor="pointer"
+        // height="100%"
+      >
+        <Text
+          fontSize="sm"
+          fontWeight={500}
+          overflow="hidden"
+          textOverflow="clip"
+          whiteSpace="nowrap"
+        >
+          {event.title}
+        </Text>
+        <Text fontSize="xs">{event.extendedProps.room}</Text>
+      </Box>
+    );
   };
 
   return (
@@ -56,10 +51,14 @@ export function Calendar(props: {
       allDaySlot={false}
       dayHeaderFormat={{ weekday: "short" }}
       editable={false}
-      events={events}
+      events={selectedActivities
+        .flatMap((act) => act.events)
+        .flatMap((event) => event.eventInputs)}
       eventContent={renderEvent}
-      eventClick={handleEventClick}
-      displayEventTime={false}
+      eventClick={(e) => {
+        // extendedProps: non-standard props of {@link Event.eventInputs}
+        state.setViewedActivity(e.event.extendedProps.activity as Activity);
+      }}
       headerToolbar={false}
       height="auto"
       // a date that is, conveniently enough, a monday
@@ -77,30 +76,16 @@ export function Calendar(props: {
       slotMaxTime="22:00:00"
       weekends={false}
       selectable={viewedActivity instanceof NonClass}
-      select={handleSelect}
+      select={(e) => {
+        viewedActivity instanceof NonClass &&
+          state.addTimeslot(
+            viewedActivity,
+            Timeslot.fromStartEnd(
+              Slot.fromStartDate(e.start),
+              Slot.fromStartDate(e.end),
+            ),
+          );
+      }}
     />
   );
 }
-
-const renderEvent = ({ event }: EventContentArg) => {
-  return (
-    <Box
-      p={0.5}
-      lineHeight={1.3}
-      cursor="pointer"
-      color={event.textColor}
-      height="100%"
-    >
-      <Text
-        fontSize="sm"
-        fontWeight={500}
-        overflow="hidden"
-        textOverflow="clip"
-        whiteSpace="nowrap"
-      >
-        {event.title}
-      </Text>
-      <Text fontSize="xs">{event.extendedProps.room}</Text>
-    </Box>
-  );
-};
