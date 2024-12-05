562c56d85eeac7016b3a007a7c8b8499a725c259
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-12-05 18:42:14 -0500

    Fix tooltip in button looking weird in groups

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,8 +4,10 @@ import {
   Button as ChakraButton,
   Span,
   Spinner,
+  Tooltip as ChakraTooltip,
 } from "@chakra-ui/react";
 import * as React from "react";
+import { Tooltip } from "./tooltip";
 
 interface ButtonLoadingProps {
   loading?: boolean;
@@ -45,3 +47,27 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     );
   },
 );
+
+export interface LabelledButtonProps extends ButtonProps {
+  showArrow?: boolean;
+  portalled?: boolean;
+  portalRef?: React.RefObject<HTMLElement>;
+  titleProps?: ChakraTooltip.ContentProps;
+  disabled?: boolean;
+}
+
+export const LabelledButton = (props: LabelledButtonProps) => {
+  const { showArrow, title, titleProps, portalled, disabled, ...rest } = props;
+  if (!title) return <Button {...rest} />;
+  return (
+    <Tooltip
+      content={title}
+      contentProps={titleProps}
+      portalled={portalled}
+      showArrow={showArrow}
+      disabled={disabled}
+    >
+      <Button {...rest} />
+    </Tooltip>
+  );
+};
