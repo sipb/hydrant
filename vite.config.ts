2e69a01593b1d6bf503158f144d9a09fe3db5514
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-12-08 04:58:05 -0500

    add type checker to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), checker({ typescript: true })],
 });
