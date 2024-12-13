ee474501033aaf0070b98f2ba92380bdea9aa42e
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-12-13 12:04:19 -0500

    Add polyfill to fix build (#102)
    Buffer isn't externalized by default. This can probably be tweaked in a
    bit, but I'm gonna merge now since it can't run on prod without this for
    now.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import checker from "vite-plugin-checker";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    nodePolyfills(),
     checker({
       typescript: true,
       eslint: { lintCommand: "eslint .", useFlatConfig: true },
