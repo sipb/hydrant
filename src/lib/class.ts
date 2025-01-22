0fa674d53872c76b47d4dc4c76e873521f5ce169
suufi <12759332+suufi@users.noreply.github.com>
2025-01-22 10:45:46 -0600

    OpenGrades Integration
    Fixes #117

--- src/lib/class.ts
+++ src/lib/class.ts
@@ -480,6 +480,10 @@ export class Class {
         label: "Class Evaluations",
         url: `https://sisapp.mit.edu/ose-rpt/subjectEvaluationSearch.htm?search=Search&subjectCode=${this.number}`,
       },
+      {
+        label: "Course Data on OpenGrades",
+        url: `https://opengrades.mit.edu/classes/aggregate/${this.number}?utm_source=hydrant`
+      }
     ];
 
     if (this.oldNumber) {
