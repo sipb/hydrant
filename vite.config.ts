f3d2fd3d2958fa9514c46d662b78659b7cde0e1b
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-03-03 20:35:23 -0500

    Updates packages, adds dependabot file (#159)
    Resolves #158

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import checker from "vite-plugin-checker";
