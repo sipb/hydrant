aabdd5e048a89f42bc91c70eec6bbc64ec03616e
Pratyush Venkatakrishnan <contact@psvenk.com>
2025-05-05 00:52:29 -0400

    Complete overrides form (but submit WIP)

--- src/components/OverridesApp.tsx
+++ src/components/OverridesApp.tsx
@@ -1,14 +1,141 @@
 import { Flex, Heading, Image } from "@chakra-ui/react";
+import { useState } from "react";
+
 import { JsonForms } from "@jsonforms/react";
-import { materialCells, materialRenderers } from "@jsonforms/material-renderers";
+import {
+  materialCells,
+  materialRenderers,
+} from "@jsonforms/material-renderers";
+import Button from "@mui/material/Button";
 
 import { Provider } from "./ui/provider";
 
 import logo from "../assets/logo.svg";
 import schema from "../../scrapers/overrides.toml.d/override-schema.json";
 
+schema.required = ["number"];
+
+const uischema = {
+  type: "VerticalLayout",
+  elements: [
+    {
+      type: "HorizontalLayout",
+      elements: [
+        { type: "Control", scope: "#/properties/number" },
+        { type: "Control", scope: "#/properties/name" },
+      ],
+    },
+    {
+      type: "HorizontalLayout",
+      elements: [
+        {
+          type: "Control",
+          label: "Formerly known as",
+          scope: "#/properties/oldNumber",
+        },
+        {
+          type: "Control",
+          label: "Same as",
+          scope: "#/properties/same",
+        },
+        {
+          type: "Control",
+          label: "Meets with",
+          scope: "#/properties/meets",
+        },
+      ],
+    },
+    { type: "Control", label: "Prerequisites", scope: "#/properties/prereqs" },
+    {
+      type: "HorizontalLayout",
+      elements: [
+        { type: "Control", scope: "#/properties/level" },
+        {
+          type: "Control",
+          scope: "#/properties/lectureUnits",
+          rule: {
+            effect: "DISABLE",
+            condition: {
+              scope: "#/properties/isVariableUnits",
+              schema: { const: true },
+            },
+          },
+        },
+        {
+          type: "Control",
+          scope: "#/properties/labUnits",
+          rule: {
+            effect: "DISABLE",
+            condition: {
+              scope: "#/properties/isVariableUnits",
+              schema: { const: true },
+            },
+          },
+        },
+        {
+          type: "Control",
+          scope: "#/properties/preparationUnits",
+          rule: {
+            effect: "DISABLE",
+            condition: {
+              scope: "#/properties/isVariableUnits",
+              schema: { const: true },
+            },
+          },
+        },
+        {
+          type: "Control",
+          label: "Units arranged",
+          scope: "#/properties/isVariableUnits",
+        },
+      ],
+    },
+    {
+      type: "Control",
+      scope: "#/properties/description",
+      options: { multi: true },
+    },
+    {
+      type: "HorizontalLayout",
+      elements: [
+        { type: "Control", label: "HASS-H", scope: "#/properties/hassH" },
+        { type: "Control", label: "HASS-A", scope: "#/properties/hassA" },
+        { type: "Control", label: "HASS-S", scope: "#/properties/hassS" },
+        { type: "Control", label: "HASS-E", scope: "#/properties/hassE" },
+        {
+          type: "Control",
+          label: "CI-H",
+          scope: "#/properties/cih",
+          rule: {
+            effect: "DISABLE",
+            condition: {
+              scope: "#/properties/cihw",
+              schema: { const: true },
+            },
+          },
+        },
+        {
+          type: "Control",
+          label: "CI-HW",
+          scope: "#/properties/cihw",
+          rule: {
+            effect: "DISABLE",
+            condition: {
+              scope: "#/properties/cih",
+              schema: { const: true },
+            },
+          },
+        },
+      ],
+    },
+    { type: "Control", label: "Instructors", scope: "#/properties/inCharge" },
+    { type: "Control", label: "URL", scope: "#/properties/url" },
+  ],
+};
+
 /** The main application. */
 export default function App() {
+  const [data, setData] = useState({});
   return (
     <Provider>
       <Flex direction="column" gap={5} px="20%" py={4}>
@@ -24,9 +151,20 @@ export default function App() {
         <Heading>Submit Overrides</Heading>
         <JsonForms
           schema={schema}
+          uischema={uischema}
           renderers={materialRenderers}
           cells={materialCells}
+          onChange={({ data, _errors }) => setData(data)}
         />
+        <Button
+          variant="contained"
+          onClick={() => {
+            // TODO convert data to TOML, compose email
+            console.log(data);
+          }}
+        >
+          Submit
+        </Button>
       </Flex>
     </Provider>
   );
