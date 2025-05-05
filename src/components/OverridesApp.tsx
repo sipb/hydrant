4e45d0020467b6972457c37ac9440bbc05aed7bf
Pratyush Venkatakrishnan <contact@psvenk.com>
2025-05-05 01:07:56 -0400

    Allow submitting multiple overrides at once

--- src/components/OverridesApp.tsx
+++ src/components/OverridesApp.tsx
@@ -11,131 +11,147 @@ import Button from "@mui/material/Button";
 import { Provider } from "./ui/provider";
 
 import logo from "../assets/logo.svg";
-import schema from "../../scrapers/overrides.toml.d/override-schema.json";
+import itemSchema from "../../scrapers/overrides.toml.d/override-schema.json";
 
-schema.required = ["number"];
+itemSchema.required = ["number"];
+const schema = {
+  title: "Overrides",
+  type: "array",
+  items: itemSchema,
+  $defs: {
+    ...itemSchema.$defs,
+  },
+};
 
 const uischema = {
-  type: "VerticalLayout",
-  elements: [
-    {
-      type: "HorizontalLayout",
-      elements: [
-        { type: "Control", scope: "#/properties/number" },
-        { type: "Control", scope: "#/properties/name" },
-      ],
-    },
-    {
-      type: "HorizontalLayout",
+  type: "Control",
+  scope: "#",
+  options: {
+    detail: {
+      type: "VerticalLayout",
       elements: [
         {
-          type: "Control",
-          label: "Formerly known as",
-          scope: "#/properties/oldNumber",
+          type: "HorizontalLayout",
+          elements: [
+            { type: "Control", scope: "#/properties/number" },
+            { type: "Control", scope: "#/properties/name" },
+          ],
         },
         {
-          type: "Control",
-          label: "Same as",
-          scope: "#/properties/same",
+          type: "HorizontalLayout",
+          elements: [
+            {
+              type: "Control",
+              label: "Formerly known as",
+              scope: "#/properties/oldNumber",
+            },
+            {
+              type: "Control",
+              label: "Same as",
+              scope: "#/properties/same",
+            },
+            {
+              type: "Control",
+              label: "Meets with",
+              scope: "#/properties/meets",
+            },
+          ],
         },
+        { type: "Control", label: "Prerequisites", scope: "#/properties/prereqs" },
         {
-          type: "Control",
-          label: "Meets with",
-          scope: "#/properties/meets",
-        },
-      ],
-    },
-    { type: "Control", label: "Prerequisites", scope: "#/properties/prereqs" },
-    {
-      type: "HorizontalLayout",
-      elements: [
-        { type: "Control", scope: "#/properties/level" },
-        {
-          type: "Control",
-          scope: "#/properties/lectureUnits",
-          rule: {
-            effect: "DISABLE",
-            condition: {
+          type: "HorizontalLayout",
+          elements: [
+            { type: "Control", scope: "#/properties/level" },
+            {
+              type: "Control",
+              scope: "#/properties/lectureUnits",
+              rule: {
+                effect: "DISABLE",
+                condition: {
+                  scope: "#/properties/isVariableUnits",
+                  schema: { const: true },
+                },
+              },
+            },
+            {
+              type: "Control",
+              scope: "#/properties/labUnits",
+              rule: {
+                effect: "DISABLE",
+                condition: {
+                  scope: "#/properties/isVariableUnits",
+                  schema: { const: true },
+                },
+              },
+            },
+            {
+              type: "Control",
+              scope: "#/properties/preparationUnits",
+              rule: {
+                effect: "DISABLE",
+                condition: {
+                  scope: "#/properties/isVariableUnits",
+                  schema: { const: true },
+                },
+              },
+            },
+            {
+              type: "Control",
+              label: "Units arranged",
               scope: "#/properties/isVariableUnits",
-              schema: { const: true },
             },
-          },
+          ],
         },
         {
           type: "Control",
-          scope: "#/properties/labUnits",
-          rule: {
-            effect: "DISABLE",
-            condition: {
-              scope: "#/properties/isVariableUnits",
-              schema: { const: true },
-            },
-          },
+          scope: "#/properties/description",
+          options: { multi: true },
         },
         {
-          type: "Control",
-          scope: "#/properties/preparationUnits",
-          rule: {
-            effect: "DISABLE",
-            condition: {
-              scope: "#/properties/isVariableUnits",
-              schema: { const: true },
-            },
-          },
-        },
-        {
-          type: "Control",
-          label: "Units arranged",
-          scope: "#/properties/isVariableUnits",
-        },
-      ],
-    },
-    {
-      type: "Control",
-      scope: "#/properties/description",
-      options: { multi: true },
-    },
-    {
-      type: "HorizontalLayout",
-      elements: [
-        { type: "Control", label: "HASS-H", scope: "#/properties/hassH" },
-        { type: "Control", label: "HASS-A", scope: "#/properties/hassA" },
-        { type: "Control", label: "HASS-S", scope: "#/properties/hassS" },
-        { type: "Control", label: "HASS-E", scope: "#/properties/hassE" },
-        {
-          type: "Control",
-          label: "CI-H",
-          scope: "#/properties/cih",
-          rule: {
-            effect: "DISABLE",
-            condition: {
-              scope: "#/properties/cihw",
-              schema: { const: true },
-            },
-          },
-        },
-        {
-          type: "Control",
-          label: "CI-HW",
-          scope: "#/properties/cihw",
-          rule: {
-            effect: "DISABLE",
-            condition: {
+          type: "HorizontalLayout",
+          elements: [
+            { type: "Control", label: "HASS-H", scope: "#/properties/hassH" },
+            { type: "Control", label: "HASS-A", scope: "#/properties/hassA" },
+            { type: "Control", label: "HASS-S", scope: "#/properties/hassS" },
+            { type: "Control", label: "HASS-E", scope: "#/properties/hassE" },
+            {
+              type: "Control",
+              label: "CI-H",
               scope: "#/properties/cih",
-              schema: { const: true },
+              rule: {
+                effect: "DISABLE",
+                condition: {
+                  scope: "#/properties/cihw",
+                  schema: { const: true },
+                },
+              },
             },
-          },
+            {
+              type: "Control",
+              label: "CI-HW",
+              scope: "#/properties/cihw",
+              rule: {
+                effect: "DISABLE",
+                condition: {
+                  scope: "#/properties/cih",
+                  schema: { const: true },
+                },
+              },
+            },
+          ],
         },
+        { type: "Control", label: "Instructors", scope: "#/properties/inCharge" },
+        { type: "Control", label: "URL", scope: "#/properties/url" },
       ],
     },
-    { type: "Control", label: "Instructors", scope: "#/properties/inCharge" },
-    { type: "Control", label: "URL", scope: "#/properties/url" },
-  ],
+  },
 };
+
+
 
 /** The main application. */
 export default function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   return (
     <Provider>
       <Flex direction="column" gap={5} px="20%" py={4}>
