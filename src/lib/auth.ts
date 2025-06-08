a935cf0f28b26ca1f4f0b5eaa17ffa5537a3f84b
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-06-08 16:24:22 -0400

    add helpers for favorite courses

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,3 +37,59 @@ export const { getSession, commitSession, destroySession } =
 export const SessionContext = createContext<Awaited<
   ReturnType<typeof getSession>
 > | null>(null);
+
+// API FUNCTION CALLS
+
+export const getFavoriteCourses = async (authToken: string) => {
+  const response = await fetch(`${FIREROAD_URL}/prefs/favorites`, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch favorite courses");
+  }
+
+  const result = (await response.json()) as
+    | {
+        success: false;
+        error: string;
+      }
+    | { success: true; favorites: string[] };
+
+  if (!result.success) {
+    throw new Error("Failed to fetch favorite courses: " + result.error);
+  }
+
+  return result.favorites;
+};
+
+export const setFavoriteCourses = async (
+  authToken: string,
+  favorites: string[],
+) => {
+  const response = await fetch(`${FIREROAD_URL}/prefs/favorites`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${authToken}`,
+    },
+    body: JSON.stringify(favorites),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to set favorite courses");
+  }
+
+  const result = (await response.json()) as
+    | {
+        success: false;
+        error: string;
+      }
+    | { success: true };
+
+  if (!result.success) {
+    throw new Error("Failed to set favorite courses: " + result.error);
+  }
+};
