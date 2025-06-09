fa8326a6b89b537c39bace3a17b27ceb66e92dff
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2025-06-09 18:12:10 -0400

    nicer auth button

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,32 +1,33 @@
 import { IconButton } from "@chakra-ui/react";
 import { Tooltip } from "./ui/tooltip";
 
-import { LuUser } from "react-icons/lu";
+import { LuLogIn, LuLogOut } from "react-icons/lu";
 import { Link } from "react-router";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { SessionContext } from "../lib/auth";
 
 export function AuthButton() {
   const session = useContext(SessionContext);
-  const username = session?.get("academic_id");
+  const username = useMemo(() => session?.get("academic_id"), [session]);
 
-  return (
+  return username ? (
+    // LOGGED IN
     <Tooltip
-      content={
-        username
-          ? `Welcome ${username.split("@")[0]}! Click here to log out.`
-          : "Click to log in!"
-      }
+      content={`Welcome ${username.split("@")[0]}! Click here to log out.`}
     >
-      <IconButton
-        aria-label="Login"
-        size="sm"
-        variant="outline"
-        as={(props) => (
-          <Link to={"/auth/" + (username ? "logout" : "login")} {...props} />
-        )}
-      >
-        <LuUser />
+      <IconButton aria-label="Logout" size="sm" variant="outline" asChild>
+        <Link to="/auth/logout">
+          <LuLogOut />
+        </Link>
+      </IconButton>
+    </Tooltip>
+  ) : (
+    // LOGGED OUT
+    <Tooltip content="Click to log in!">
+      <IconButton aria-label="Login" size="sm" variant="outline" asChild>
+        <Link to="/auth/login">
+          <LuLogIn />
+        </Link>
       </IconButton>
     </Tooltip>
   );
