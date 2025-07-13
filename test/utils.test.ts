2af5cfc6b6981b5b2dee25d6a7d82211a37ea8ac
duck_master <duckeagle1280@gmail.com>
2025-07-13 15:24:29 -0400

    implement sum tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,8 +1,24 @@
 import assert from "node:assert";
-import { test } from "node:test";
+import { describe, test } from "node:test";
+import { sum } from "../src/lib/utils.jsx"; // NOTE: you MUST use `tsx` to run this, since we're importing a tsx file
 
 test("example", (t) => {
   assert.strictEqual(1, 1);
 });
 
-// TODO: add real tests
+describe("sum", () => {
+  /* partition: length 0, 1, >1
+     NOTE: test cases are intentionally chosen somewhat randomly 
+     */
+  test("length 0", () => {
+    assert.strictEqual(sum([]), 0);
+  });
+
+  test("length 1", () => {
+    assert.strictEqual(sum([15]), 15);
+  });
+
+  test("length >1", () => {
+    assert.strictEqual(sum([9, 1, 1, 8]), 19);
+  });
+});
