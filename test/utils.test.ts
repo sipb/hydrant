7055f3da390d4b95d26a689fa83b94e07278f8aa
duck_master <duckeagle1280@gmail.com>
2025-07-13 17:36:01 -0400

    add simplifyString tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,11 @@
 import assert from "node:assert";
 import { describe, test } from "node:test";
-import { sum, urlencode, urldecode } from "../src/lib/utils.jsx"; // NOTE: you MUST use `tsx` to run this, since we're importing a tsx file
+import {
+  sum,
+  urlencode,
+  urldecode,
+  simplifyString,
+} from "../src/lib/utils.jsx"; // NOTE: you MUST use `tsx` to run this, since we're importing a tsx file
 
 test("example", (t) => {
   assert.strictEqual(1, 1);
@@ -34,4 +39,34 @@ test("urlencode followed by urldecode", () => {
     urldecode(urlencode(someRandomTestData)),
     someRandomTestData,
   );
+});
+
+describe("simplifyString", () => {
+  /**
+   * Partition on characters used:
+   * - has uppercase letters: yes, no
+   * - has non-alphanumeric symbols: yes, no
+   */
+  test("keeps lowercase only same", () => {
+    assert.strictEqual(simplifyString("hi"), "hi");
+  });
+
+  test("keeps number same", () => {
+    assert.strictEqual(simplifyString("42"), "42");
+  });
+
+  test("has uppercase letters, no non-alphanumeric symbols", () => {
+    assert.strictEqual(simplifyString("Hello42"), "hello42");
+  });
+
+  test("has non-alphanumeric symbols, no uppercase letters", () => {
+    assert.strictEqual(simplifyString(".1223h9f obanboit"), "1223h9fobanboit");
+  });
+
+  test("has non-alphanumeric symbols and uppercase letters", () => {
+    assert.strictEqual(
+      simplifyString("OH) U$oiu262 HUG*$ uIo"),
+      "ohuoiu262huguio",
+    );
+  });
 });
