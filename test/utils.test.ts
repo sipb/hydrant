a86ee26380f307e6379b35a23d32d13523971df0
duck_master <duckeagle1280@gmail.com>
2025-07-19 00:43:59 -0400

    await all tests in utils.test.ts; remove example test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -9,11 +9,8 @@ import {
   simplifyString,
 } from "../src/lib/utils.jsx"; // NOTE: you MUST use `tsx` to run this, since we're importing a tsx file
 
-test("example", (t) => {
-  assert.strictEqual(1, 1);
-});
-
-describe("classSort", () => {
+// NOTE: you MUST use `await` (to satisfy ESLint), because `describe` and `test` return promises
+await describe("classSort", async () => {
   /**
    * Partition:
    * - a & b both null/undefined
@@ -34,125 +31,125 @@ describe("classSort", () => {
    * - course numbers & letters match, bGroups.classNumber > aGroups.classNumber
    * - everything matches
    */
-  test("a and b both null", () => {
+  await test("a and b both null", () => {
     assert.strictEqual(classSort(null, null), 0);
   });
 
-  test("a null, b not null", () => {
+  await test("a null, b not null", () => {
     assert.strictEqual(classSort(null, "lorem"), 1);
   });
 
-  test("b null, a not null", () => {
+  await test("b null, a not null", () => {
     assert.strictEqual(classSort("ipsum", null), -1);
   });
 
-  test("a & b both invalid", () => {
+  await test("a & b both invalid", () => {
     assert.strictEqual(classSort("HeLlO", "wOrLd"), 0);
   });
 
-  test("a invalid, b valid", () => {
+  await test("a invalid, b valid", () => {
     assert.strictEqual(classSort("dolor", "1A.2B"), 0);
   });
 
-  test("a valid, b invalid", () => {
+  await test("a valid, b invalid", () => {
     assert.strictEqual(classSort("3C.4D", "sit"), 0);
   });
 
-  test("a no course number, b has course number", () => {
+  await test("a no course number, b has course number", () => {
     assert.strictEqual(classSort("A.1", "1.234"), 1);
   });
 
-  test("b no course number, a has course number", () => {
+  await test("b no course number, a has course number", () => {
     assert.strictEqual(classSort("5.678", "B.9"), -1);
   });
 
-  test("a has greater course number than b", () => {
+  await test("a has greater course number than b", () => {
     assert.strictEqual(classSort("1.23", "0.12"), 1);
   });
 
-  test("a has lesser course number than b", () => {
+  await test("a has lesser course number than b", () => {
     assert.strictEqual(classSort("1.01", "2.56"), -1);
   });
 
-  test("same course number, a has greater course letter than b", () => {
+  await test("same course number, a has greater course letter than b", () => {
     assert.strictEqual(classSort("1BETA.1", "1ALPHA.2"), 1);
   });
 
-  test("same course number, a has lesser course letter than b", () => {
+  await test("same course number, a has lesser course letter than b", () => {
     assert.strictEqual(classSort("1DELTA.3", "1EPSILON.4"), -1);
   });
 
-  test("same course number and letter, a has greater class number than b", () => {
+  await test("same course number and letter, a has greater class number than b", () => {
     assert.strictEqual(classSort("AWS.208", "AWS.084"), 1);
   });
 
-  test("same course number and letter, a has lesser class number than b", () => {
+  await test("same course number and letter, a has lesser class number than b", () => {
     assert.strictEqual(classSort("1234.271", "1234.316"), -1);
   });
 
-  test("everything is identical", () => {
+  await test("everything is identical", () => {
     assert.strictEqual(classSort("4BF.261", "4BF.261"), 0);
   });
 });
 
-describe("classNumberMatch", () => {
+await describe("classNumberMatch", async () => {
   /**
    * Partition:
    * - searchString: contains ".", doesn't contain "."
    * - classNumber: identical, non-identical match, not a match
    * - exact: true, false
    */
-  test("searchString contains period, classNumber matches, exact = true", () => {
+  await test("searchString contains period, classNumber matches, exact = true", () => {
     assert.strictEqual(classNumberMatch("1.234", "1.234", true), true);
   });
 
-  test("searchString contains period, classNumber doesn't match, exact = true", () => {
+  await test("searchString contains period, classNumber doesn't match, exact = true", () => {
     assert.strictEqual(classNumberMatch("1.234", "5.678", true), false);
   });
 
-  test("searchString contains period, classNumber matches, exact = false", () => {
+  await test("searchString contains period, classNumber matches, exact = false", () => {
     assert.strictEqual(classNumberMatch("1.35", "1.357", false), true);
   });
 
-  test("searchString contains period, classNumber matches, exact = false", () => {
+  await test("searchString contains period, classNumber matches, exact = false", () => {
     assert.strictEqual(classNumberMatch("1.35", "2.46", false), false);
   });
 
-  test("searchString without period, classNumber matches, exact = true", () => {
+  await test("searchString without period, classNumber matches, exact = true", () => {
     assert.strictEqual(classNumberMatch("1234", "1.234", true), true);
   });
 
-  test("searchString without period, classNumber doesn't match, exact = true", () => {
+  await test("searchString without period, classNumber doesn't match, exact = true", () => {
     assert.strictEqual(classNumberMatch("1234", "5.678", true), false);
   });
 
-  test("searchString without period, classNumber matches, exact = false", () => {
+  await test("searchString without period, classNumber matches, exact = false", () => {
     assert.strictEqual(classNumberMatch("1A35", "1A.357", false), true);
   });
 
-  test("searchString without period, classNumber matches, exact = false", () => {
+  await test("searchString without period, classNumber matches, exact = false", () => {
     assert.strictEqual(classNumberMatch("1A35", "2B.46", false), false);
   });
 });
 
-describe("sum", () => {
+await describe("sum", async () => {
   /* partition: length 0, 1, >1
      NOTE: test cases are intentionally chosen somewhat randomly 
      */
-  test("length 0", () => {
+  await test("length 0", () => {
     assert.strictEqual(sum([]), 0);
   });
 
-  test("length 1", () => {
+  await test("length 1", () => {
     assert.strictEqual(sum([15]), 15);
   });
 
-  test("length >1", () => {
+  await test("length >1", () => {
     assert.strictEqual(sum([9, 1, 1, 8]), 19);
   });
 });
 
-test("urlencode followed by urldecode", () => {
+await test("urlencode followed by urldecode", () => {
   // arbitrarily chosen; change this to whatever you want
   const someRandomTestData: unknown = {
     lorem: 3,
@@ -165,29 +162,29 @@ test("urlencode followed by urldecode", () => {
   );
 });
 
-describe("simplifyString", () => {
+await describe("simplifyString", async () => {
   /**
    * Partition on characters used:
    * - has uppercase letters: yes, no
    * - has non-alphanumeric symbols: yes, no
    */
-  test("keeps lowercase only same", () => {
+  await test("keeps lowercase only same", () => {
     assert.strictEqual(simplifyString("hi"), "hi");
   });
 
-  test("keeps number same", () => {
+  await test("keeps number same", () => {
     assert.strictEqual(simplifyString("42"), "42");
   });
 
-  test("has uppercase letters, no non-alphanumeric symbols", () => {
+  await test("has uppercase letters, no non-alphanumeric symbols", () => {
     assert.strictEqual(simplifyString("Hello42"), "hello42");
   });
 
-  test("has non-alphanumeric symbols, no uppercase letters", () => {
+  await test("has non-alphanumeric symbols, no uppercase letters", () => {
     assert.strictEqual(simplifyString(".1223h9f obanboit"), "1223h9fobanboit");
   });
 
-  test("has non-alphanumeric symbols and uppercase letters", () => {
+  await test("has non-alphanumeric symbols and uppercase letters", () => {
     assert.strictEqual(
       simplifyString("OH) U$oiu262 HUG*$ uIo"),
       "ohuoiu262huguio",
