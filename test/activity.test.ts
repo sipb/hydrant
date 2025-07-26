5e2cfe64ee05e09883a1fbd7b37cce8633b25844
duck_master <duckeagle1280@gmail.com>
2025-07-26 00:05:36 -0400

    implement tests for Timeslot

--- test/activity.test.ts
+++ test/activity.test.ts
@@ -1,20 +1,52 @@
+import assert from "node:assert";
 import { describe, test } from "node:test";
+import { Timeslot } from "../src/lib/activity.js";
+import { Slot } from "../src/lib/dates.js";
 
 await describe("Timeslot", async () => {
-  await test.skip("Timeslot.fromStartEnd");
+  await test.skip("Timeslot.fromStartEnd", () => {
+    const myStart: Slot = new Slot(2);
+    const myEnd: Slot = new Slot(120);
+    const myTimeslot: Timeslot = Timeslot.fromStartEnd(myStart, myEnd);
+    assert.deepStrictEqual(myTimeslot.startTime, myStart.startDate);
+    assert.deepStrictEqual(myTimeslot.endSlot, myEnd);
+  });
 
-  await test.skip("Timeslot.endSlot");
+  await test("Timeslot.endSlot", () => {
+    const myTimeslot: Timeslot = new Timeslot(131, 4); // numbers generated with the help of an RNG; adjust as needed
+    assert.deepStrictEqual(myTimeslot.endSlot, new Slot(135));
+  });
 
-  await test.skip("Timeslot.startTime");
+  await test("Timeslot.startTime", () => {
+    const myTimeslot: Timeslot = new Timeslot(22, 43); // note: slot 22 is Monday at 5:00 PM
+    assert.deepStrictEqual(
+      myTimeslot.startTime,
+      new Date(2001, 0, 1, 17, 0, 0, 0),
+    );
+  });
 
-  await test.skip("Timeslot.endTime");
+  await test("Timeslot.endTime", () => {
+    const myTimeslot: Timeslot = new Timeslot(79, 52); // note: slot 79 + 52 = 131 is Thursday at 8:30 PM
+    assert.deepStrictEqual(
+      myTimeslot.endTime,
+      new Date(2001, 0, 4, 20, 30, 0, 0),
+    );
+  });
 
-  await test.skip("Timeslot.hours");
+  await test("Timeslot.hours", () => {
+    const myTimeslot: Timeslot = new Timeslot(9, 14);
+    assert.strictEqual(myTimeslot.hours, 7);
+  });
 
+  // TODO: partition
   await test.skip("Timeslot.conflicts");
 
-  await test.skip("Timeslot.toString");
+  await test("Timeslot.toString", () => {
+    const myTimeslot: Timeslot = new Timeslot(151, 5);
+    assert.strictEqual(myTimeslot.toString(), "Fri, 1:30 PM – 4:00 PM");
+  });
 
+  // TODO: partition
   await test.skip("Timeslot.equals");
 });
 
