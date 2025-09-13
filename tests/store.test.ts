4f8e8fba6bbbe6e79cef658c6f087b13d7872dd7
duck_master <duckeagle1280@gmail.com>
2025-09-13 00:15:51 -0400

    write Store.toKey tests

--- tests/store.test.ts
+++ tests/store.test.ts
@@ -1,5 +1,6 @@
 import { expect, describe, test } from "vitest";
 import { LocalStorage } from "node-localstorage";
+import { Store } from "../src/lib/store.js";
 
 test("node-localstorage sanity check", () => {
   expect(LocalStorage).toBeDefined();
@@ -10,7 +11,18 @@ test("node-localstorage sanity check", () => {
 
 // TODO
 describe("Store", () => {
-  test.skip("Store.toKey");
+  // create a Store object once and for all, since they touch the same localStorage anyways
+  const myStore: Store = new Store("lorem");
+
+  describe("Store.toKey", () => {
+    test("store.toKey with global = true", () => {
+      expect(myStore.toKey("ipsum", true)).toStrictEqual("ipsum");
+    });
+
+    test("store.toKey with global = false", () => {
+      expect(myStore.toKey("ipsum", false)).toStrictEqual("lorem-ipsum");
+    });
+  });
 
   test.skip("Store.get");
 
