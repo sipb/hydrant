2eb2417f24264d54d5707709a461eb463bd5c38e
duck_master <duckeagle1280@gmail.com>
2025-09-12 23:10:54 -0400

    add outline for testing src/lib/store.ts

---
+++ tests/store.test.ts
@@ -0,0 +1,14 @@
+import { describe, test } from "vitest";
+
+// TODO: find out some way to mock `localStorage`
+describe("Store", () => {
+  test.skip("Store.toKey");
+
+  test.skip("Store.get");
+
+  test.skip("Store.globalGet");
+
+  test.skip("Store.set");
+
+  test.skip("Store.globalSet");
+});
