999c462b739542082ba7dd5f4911e3b7e5b94ca9
duck_master <duckeagle1280@gmail.com>
2025-09-14 00:39:36 -0400

    uninstall node-localstorage since jsdom actually supports localStorage itself

--- tests/store.test.ts
+++ tests/store.test.ts
@@ -1,15 +1,8 @@
 import { expect, describe, test } from "vitest";
-import { LocalStorage } from "node-localstorage";
 import { Store } from "../src/lib/store.js";
 
-test("node-localstorage sanity check", () => {
-  expect(LocalStorage).toBeDefined();
-  const myLocalStorage: Storage = new LocalStorage("./storage.txt");
-  expect(myLocalStorage).toBeDefined();
-  myLocalStorage._deleteLocation();
-});
-
-// TODO
+// TODO: write these tests
+// note that jsdom supports localStorage since v11.12.0; c.f. github.com/jsdom/jsdom/blob/main/Changelog.md
 describe("Store", () => {
   // create a Store object once and for all, since they touch the same localStorage anyways
   const myStore: Store = new Store("lorem");
