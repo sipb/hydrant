ef085cdf26912760e8c29d95788aad51eb9ccf7c
duck_master <duckeagle1280@gmail.com>
2025-09-15 19:42:11 -0400

    add afterEach hook, localStorage sanity check tests

--- tests/store.test.ts
+++ tests/store.test.ts
@@ -1,4 +1,4 @@
-import { expect, describe, test } from "vitest";
+import { afterEach, expect, describe, test } from "vitest";
 import { Store } from "../src/lib/store.js";
 
 // TODO: write these tests
@@ -6,6 +6,10 @@ import { Store } from "../src/lib/store.js";
 describe("Store", () => {
   // create a Store object once and for all, since they touch the same localStorage anyways
   const myStore: Store = new Store("lorem");
+
+  afterEach(() => {
+    localStorage.clear();
+  });
 
   describe("Store.toKey", () => {
     test("store.toKey with global = true", () => {
@@ -15,6 +19,19 @@ describe("Store", () => {
     test("store.toKey with global = false", () => {
       expect(myStore.toKey("ipsum", false)).toStrictEqual("lorem-ipsum");
     });
+  });
+
+  // double check that localStorage really works
+  test("localStorage sanity check", () => {
+    expect(localStorage.length).toStrictEqual(0);
+    localStorage.setItem("lorem", "ipsum");
+    expect(localStorage.getItem("lorem")).toStrictEqual("ipsum");
+    expect(localStorage.length).toStrictEqual(1);
+  });
+
+  test("localStorage sanity check 2", () => {
+    // if afterEach hook is disabled, this will fail because the storage hasn't been cleared
+    expect(localStorage.length).toStrictEqual(0);
   });
 
   test.skip("Store.get");
