97d3cb06c702958372d8eb0e84eb68edc6d22205
duck_master <duckeagle1280@gmail.com>
2025-09-22 13:57:42 -0400

    add outline for testing state.ts

---
+++ tests/state.test.ts
@@ -0,0 +1,75 @@
+import { describe, test } from "vitest";
+
+describe("State", () => {
+  test.skip("State.constructor");
+
+  test.skip("State.selectedActivites");
+
+  test.skip("State.colorScheme");
+
+  test.skip("State.setViewedActivity");
+
+  test.skip("State.isSelectedActivity");
+
+  test.skip("State.addActivity");
+
+  test.skip("State.removeActivity");
+
+  test.skip("State.toggleActivity");
+
+  test.skip("State.setBackgroundColor");
+
+  test.skip("State.lockSection");
+
+  test.skip("State.renameNonClass");
+
+  test.skip("State.relocateNonClass");
+
+  test.skip("State.addTimeslot");
+
+  test.skip("State.removeTimeslot");
+
+  test.skip("State.updateState");
+
+  test.skip("State.selectOption");
+
+  test.skip("State.updateActivities");
+
+  test.skip("State.fitsSchedule");
+
+  test.skip("State.setPreferences");
+
+  test.skip("State.toggleStarClass");
+
+  test.skip("State.isClassStarred");
+
+  test.skip("State.getStarredClasses");
+
+  test.skip("get State.showFeedback");
+
+  test.skip("set State.showFeedback");
+  
+  test.skip("State.reset");
+
+  test.skip("State.deflate");
+
+  test.skip("State.inflate");
+
+  test.skip("State.loadSave");
+
+  test.skip("State.storeSave");
+
+  test.skip("State.addSave");
+
+  test.skip("State.renameSave");
+
+  test.skip("State.removeSave");
+
+  test.skip("State.urlify");
+
+  test.skip("set State.defaultSchedule");
+
+  test.skip("get State.defaultSchedule");
+
+  test.skip("State.initState");
+});
