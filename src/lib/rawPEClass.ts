14f9d4b074e713e8a2db40585c18f82138810119
Pratyush Venkatakrishnan <contact@psvenk.com>
2026-01-22 18:03:54 -0500

    Add RawPEClass for PE classes

---
+++ src/lib/rawPEClass.ts
@@ -0,0 +1,34 @@
+import { type RawSection } from "./rawClass";
+
+export interface RawPEClass {
+  /** Class number; e.g., "PE.0612" */
+  number: string;
+  /** Class name; e.g., "Skate, Beginner" */
+  name: string;
+
+  /** Timeslots and locations for each section */
+  sections: RawSection[];
+  /** Raw (FireRoad format) section locations/times */
+  rawSections: string[];
+  /** Capacity per section */
+  capacity: number;
+
+  /** Start date, in ISO 8601 format */
+  startDate: string;
+  /** End date, in ISO 8601 format */
+  endDate: string;
+
+  /** PE points */
+  points: number;
+  /** Satisfies swim GIR */
+  swimGIR: boolean;
+
+  /** Prereqs, no specific format */
+  prereqs: string;
+  /** Equipment, no specific format */
+  equipment: string;
+  /** Fee, in dollars */
+  fee: number;
+  /** Description, no specific format */
+  description: string;
+}
