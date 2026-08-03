27d45f062825804c1a8f7f98eef8f82f94b8b120
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2026-08-03 01:26:30 -0400

    exclamation mark <3

--- src/components/ClassTable.tsx
+++ src/components/ClassTable.tsx
@@ -295,7 +295,7 @@ const CLASS_FLAGS_1: FilterGroup = [
   ["cih", "CI-H"],
   ["cim", "CI-M"],
   ["fits", "Fits schedule"],
-  ["new", "New"],
+  ["new", "New!"],
 ];
 
 /** List of hidden filter IDs, their displayed names, and image path, if any. */
