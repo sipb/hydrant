d5b3fca50be55af94d412967b2ebe52d8d28b4f1
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2026-09-21 20:29:19 -0400

    updatesss

--- src/components/ui/radio.tsx
+++ src/components/ui/radio.tsx
@@ -21,5 +21,4 @@ export const Radio = forwardRef<HTMLInputElement, RadioProps>(
   },
 );
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const RadioGroup = ChakraRadioGroup.Root;
